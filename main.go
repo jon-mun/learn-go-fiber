dc41fe7cde7d0d138f0bd95dbd23563840e33f64
jon-mun <jonathan.edmund@gmail.com>
2024-01-28 15:14:11 +0700

    feat: connect to db

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/jon-mun/learn-go-fiber/database"
 )
 
 func main() {
 	// Create a new Fiber instance
 	app := fiber.New()
+
+	// Connect to database
+	database.ConnectDB()
 
 	// Define a route
 	app.Get("/", func(c *fiber.Ctx) error {
