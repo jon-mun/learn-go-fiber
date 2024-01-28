3e1236c61de913f6f41795e966cfdf433da26931
jon-mun <jonathan.edmund@gmail.com>
2024-01-28 14:21:24 +0700

    init: initialize go and fiber

---
+++ main.go
@@ -0,0 +1,21 @@
+// main.go
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// Create a new Fiber instance
+	app := fiber.New()
+
+	// Define a route
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(map[string]string{
+			"message": "Hello, Fiber3!",
+		})
+	})
+
+	// Start the server on port 8080
+	app.Listen(":8080")
+}
